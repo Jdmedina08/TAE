f91963842605ec8951f715f75c891b341298306d
Juan Diego Medina Valderrama <juand@192.168.1.103>
2021-04-06 19:22:29 -0500

    First commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int suma, resta;
+        suma = sum(1,9);
+        resta = subtract(2,7);
+        System.out.println(suma+" "+resta);
+    }
+    public static int sum(int a, int b){
+        int c = a+b;
+        return c;
+    }
+    public static int subtract(int a, int b){
+        int c = a-b;
+        return c;
+    }
+}
